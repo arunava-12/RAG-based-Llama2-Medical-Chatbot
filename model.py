4eab559820baa720029986512b88a9ceced3fcd5
Arunava Mondal <arunavacr7@gmail.com>
2025-08-23 16:08:10 +0530

    fix: update import path for HuggingFaceEmbeddings

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.prompts import PromptTemplate
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import CTransformers
 from langchain.chains import RetrievalQA
